16df318c6dc73d3dc2f2c1d5a70ce281004109a5
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 13:36:44 -0800

    add pseudo to stack.js - functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add a value to the stack
   };
 
   someInstance.pop = function() {
+    // remove a value from the top of the stack
   };
 
   someInstance.size = function() {
+    // return the size of the stack
+
+    // return the number of keys in storage
   };
 
   return someInstance;
 };
+
+/*
+Analogy
+  - Like a stack of pancakes on a plate
+  - you start of with 0 pancakes then you add 1, 2, 3 pancakes
+  - you can remove a pancake and you would be left with 1, 2 pancakes
+  - a total of 2 pancakes
+
+*/
