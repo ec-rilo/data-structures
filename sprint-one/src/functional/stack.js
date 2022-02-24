a0c5df876948003817506143f67a4648e6427ef4
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 15:22:39 -0800

    (stack.js - functional): remove pseudocode

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,7 +6,6 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
-    // add a value to the stack
     var prop = someInstance.size() + 1;
     storage[prop] = value;
   };
@@ -20,18 +19,8 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
-    // return the size of the stack
     return Object.keys(storage).length;
   };
 
   return someInstance;
 };
-
-/*
-Analogy
-  - Like a stack of pancakes on a plate
-  - you start of with 0 pancakes then you add 1, 2, 3 pancakes
-  - you can remove a pancake and you would be left with 1, 2 pancakes
-  - a total of 2 pancakes
-
-*/
