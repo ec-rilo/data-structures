6c7f6039d8ec893d1b2ad1e9c76b65bf59e2ef4d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 16:51:56 -0800

    (prototypal - queue.js): pass tests for .enqueue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,11 +7,17 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
-
+  if (this.size() === 0) {
+    this.storage['1'] = value;
+  } else {
+    var keys = Object.keys(this.storage);
+    var lastKey = keys[keys.length - 1];
+    this.storage[lastKey + 1] = value;
+  }
 };
 
 queueMethods.dequeue = function() {
-
+  // remove elements from front of queue
 };
 
 queueMethods.size = function() {
