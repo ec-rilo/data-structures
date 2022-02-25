d650d44d1e3ba15a7872fabf8d8d55decf29c6c9
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 22:09:05 -0800

    (es6 - queue.js): code skeleton of Queue class

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,18 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    // add item to the back of the data structure
+  }
+
+  dequeue() {
+    // removes the item from the front of the data structure
+  }
+
+  size() {
+    // return the size of the data structure
+  }
 }
