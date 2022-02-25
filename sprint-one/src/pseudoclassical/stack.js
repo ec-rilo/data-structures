7a50e51d31d984636f65182916b9cf192b635479
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 20:04:16 -0800

    (pseudocla - stack.js): code skeleton

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,15 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function() {
 
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+
+};
