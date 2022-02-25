4fafcb60ecccd33efe11de4eab22d6d13f30488c
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 16:14:21 -0800

    (func-shared - stack.js): add barebones code

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value) {
+  // push elemnts to the top of the stack
+  },
+  pop: function() {
+  // pop elements from the top of the stack and return the popped element
+  },
+  size: function() {
+    // return the number of elements in the stack
+  }
+};
 
 
