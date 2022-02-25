eab03f0cae04eb95d87c6ca7a472a41447b2446a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 20:07:56 -0800

    (pseudocla - stack.js): pass tests for .push

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,8 +2,9 @@ var Stack = function() {
   this.storage = {};
 };
 
-Stack.prototype.push = function() {
-
+Stack.prototype.push = function(value) {
+  var numOfKeys = Object.keys(this.storage).length;
+  this.storage[numOfKeys + 1] = value;
 };
 
 Stack.prototype.pop = function() {
