2cdf3f86262f9eae90f09ca9c50d1decc2f64f3c
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 16:43:39 -0800

    (prototypal - queue.js): create barebones code

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
 
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+};
