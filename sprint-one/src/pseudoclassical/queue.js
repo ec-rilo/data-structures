a4e032f822350e25478212fd5c966edb46a0cb95
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-24 19:24:14 -0800

    (pseudocla - queue.js): passed tests for enqueue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,8 +2,14 @@ var Queue = function() {
   this.storage = {};
 };
 
-Queue.prototype.enqueue = function() {
-
+Queue.prototype.enqueue = function(value) {
+  if (this.size() === 0) {
+    this.storage['1'] = value;
+  } else {
+    var keys = Object.keys(this.storage);
+    var lastKey = keys[keys.length - 1];
+    this.storage[Number(lastKey) + 1] = value;
+  }
 };
 
 Queue.prototype.dequeue = function() {
